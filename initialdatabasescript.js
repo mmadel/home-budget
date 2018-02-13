daee4c974fa8b4d18d6a4eadbeb38e20adfb8b98
madel <mohammed.adel82@gmail.com>
2018-02-13 20:20:17 +0200

    add initial database script

---
+++ initialdatabasescript.js
@@ -0,0 +1,3 @@
+db = db.getSiblingDB('homebudget');
+db.createCollection("lookups");
+db.lookups.insert({ "_id": ObjectId("5a52b07477848e0c4d8574e0"), "budgets": [{ "name": "Interest Income", "category": "Income" }, { "name": "Electricity", "category": "Home" }, { "name": "Internet", "category": "Home" }, { "name": "Fuel", "category": "Transportation" }, { "name": "Repairs", "category": "Transportation" }, { "name": "Doctor/Dentist", "category": "Health" }, { "name": "Medicine/Drugs", "category": "Health" }], "categories": [{ "name": "Income", "type": "INCOME" }, { "name": "Home", "type": "EXPENDITURE" }, { "name": "Transportation", "type": "EXPENDITURE" }, { "name": "Health", "type": "EXPENDITURE" }] });
